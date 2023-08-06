4d16a5ebdcf15a5ff77d36cab1307a1404b784ff
taylorhms <taylorhenriquems@gmail.com>
2023-08-06 12:52:12 -0300

    tela criação de conta

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NovaConta } from '../models/nova-conta';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  novaConta(dados: NovaConta) {
+    return this.http.post<any>(`${environment.apiUrl}/usuario`, dados);
+  }
+}
