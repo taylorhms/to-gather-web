0f0c86ba008436d1c9df81b15900954275025ade
taylorhms <taylorhenriquems@gmail.com>
2023-08-13 19:55:38 -0300

    ajustando menu lateral

--- src/app/components/tg-header/tg-header.component.ts
+++ src/app/components/tg-header/tg-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tg-header',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./tg-header.component.css']
 })
 export class TgHeaderComponent {
-
+  @Input() showMenuButton: boolean = false;
 }
